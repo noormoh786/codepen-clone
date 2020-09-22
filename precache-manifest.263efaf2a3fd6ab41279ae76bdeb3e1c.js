a47f5c881b85707ad1e199d070265081bdc1a7b5
Noor Mohammad Siddiqui <noormohammadsiddiqui94@gmail.com>
2020-09-22 21:08:57 +0530

    Updates

---
+++ precache-manifest.263efaf2a3fd6ab41279ae76bdeb3e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77f0aa0614d3661fdfb09b322b485bd",
+    "url": "/codepen-clone/index.html"
+  },
+  {
+    "revision": "3baf00144cb6c83aed11",
+    "url": "/codepen-clone/static/css/2.c46397a6.chunk.css"
+  },
+  {
+    "revision": "348b827143339e3081ff",
+    "url": "/codepen-clone/static/css/main.7a9fa172.chunk.css"
+  },
+  {
+    "revision": "3baf00144cb6c83aed11",
+    "url": "/codepen-clone/static/js/2.3af19005.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codepen-clone/static/js/2.3af19005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "348b827143339e3081ff",
+    "url": "/codepen-clone/static/js/main.4da80b7b.chunk.js"
+  },
+  {
+    "revision": "7e95b33d3c3f8b32c9c4",
+    "url": "/codepen-clone/static/js/runtime-main.3a6f25c4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/codepen-clone/precache-manifest.263efaf2a3fd6ab41279ae76bdeb3e1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codepen-clone/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
